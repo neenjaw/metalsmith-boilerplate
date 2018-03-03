9c04f418d8736395c7044ada08846189059e98c6
neenjaw <tim@neenjaw.com>
2018-03-02 20:16:28 -0700

    gulp, browsersync, metalsmith, npm scripts added

--- build.js
+++ build.js
@@ -1,12 +1,52 @@
-const metalsmith = require('metalsmith');
-const drafts = require('metalsmith-drafts');
-const markdown = require('metalsmith-markdown');
+#!/user/bin/env node
+
+/*
+Metalsmith build file.
+*/
+
+'use strict';
+
+// defaults
+const consoleLog = false;
+const devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase () !== 'production');
+const pkg = require('./package.json');
+
+// main directories
+const dir = {
+    base: __dirname + '/',
+    lib: __dirname + '/lib/',
+    source: './src/',
+    dest: './dist/'
+};
+
+//modules
+const metalsmith  = require('metalsmith');
+const drafts      = require('metalsmith-drafts');
+const markdown    = require('metalsmith-markdown');
+const wordcount   = require('metalsmith-word-count');
 const collections = require('metalsmith-collections');
-const permalinks = require('metalsmith-permalinks');
-const layouts = require('metalsmith-layouts');
-const sitemap = require('metalsmith-sitemap');
+const permalinks  = require('metalsmith-permalinks');
+const layouts     = require('metalsmith-layouts');
+const sitemap     = require('metalsmith-sitemap');
+
+const htmlmin = devBuild ? null : require('metalsmith-html-minifier');
+
 const Handlebars = require('handlebars');
-const moment = require('moment');
+const moment     = require('moment');
+
+const siteMeta = {
+    devBuild: devBuild,
+    version: pkg.version,
+    name: 'neenjaw.com',
+    description: 'Views on web development, programming, life by a neenjaw.',
+    generatorname: 'Metalsmith',
+    generatorurl: 'http://metalsmith.io/',
+    generatortitle: 'Check out Metalsmith!',
+    hostname: 'Netlify',
+    hosturl: 'https://netlify.com/',
+    hosttitle: 'Learn more about Netlify',
+    domain: devBuild ?'http://127.0.0.1': 'https://neenjaw.com'
+};
 
 Handlebars.registerHelper('is', function (value, test, options) {
     if (value === test) {
@@ -20,22 +60,11 @@ Handlebars.registerHelper('date', function (date) {
     return moment(date, 'MM-DD-YYYY').format('Do MMM \'YY');
 });
 
-metalsmith(__dirname)
-    .metadata({
-        site: {
-            name: 'neenjaw.com',
-            description: 'Views on web development, programming, life by a neenjaw.',
-            generatorname: 'Metalsmith',
-            generatorurl: 'http://metalsmith.io/',
-            generatortitle: 'Check out Metalsmith!',
-            hostname: 'Netlify',
-            hosturl: 'https://netlify.com/',
-            hosttitle: 'Learn more about Netlify'
-        }
-    })
-    .source('./src')
-    .destination('./dist')
-    .clean(true)
+const ms = metalsmith(dir.base)
+    .metadata(siteMeta)
+    .source(dir.source)
+    .destination(dir.dest)
+    .clean(!devBuild)
     .use(drafts())
     .use(collections({
         posts: {
@@ -49,15 +78,24 @@ metalsmith(__dirname)
         }
     }))
     .use(markdown())
-    .use(permalinks())
+    .use(permalinks({
+        pattern: ':mainCollection/:title'
+    }))
+    .use(wordcount({
+        raw: true
+    }))
     .use(layouts({
         engine: 'handlebars',
         directory: 'layouts',
         default: 'default.hbs',
         partials: 'layouts/partials'
-    }))
+    }));
+
+if (htmlmin) ms.use(htmlmin()); // minify html
+
+ms
     .use(sitemap({
-        hostname: 'https://neenjaw.com'
+        hostname: siteMeta.domain
     }))
     .build(function (err) {
         if (err) throw err;
