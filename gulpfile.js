6d067e9851ed3fcf32719af96145aa0fa40b4bdc
Tim Austin <tim@neenjaw.com>
2020-07-21 17:24:36 -0600

    gulp dependency updated.  Blog updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,34 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var run = require('gulp-run-command').default;
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var run = require("gulp-run-command").default;
 
 // Static server
-gulp.task('serve', function () {
-    browserSync.init({
-        server: './dist/',
-        files: ['./src/' + '**/*']
-    });
+gulp.task("serve", function () {
+  browserSync.init({
+    server: "./dist/",
+    files: ["./src/" + "**/*"],
+  });
 
-    gulp.watch('./layouts/**/*.hbs', gulp.series('dev-makebuild'));
-    gulp.watch('./src/*.md', gulp.series('dev-makebuild'));
-    gulp.watch('./src/posts/*.md', gulp.series('dev-makebuild'));
+  gulp.watch("./layouts/**/*.hbs", gulp.series("dev-makebuild"));
+  gulp.watch("./src/*.md", gulp.series("dev-makebuild"));
+  gulp.watch("./src/posts/*.md", gulp.series("dev-makebuild"));
 
-    gulp.watch('./assets/css/*', gulp.series('dev-minify:css'));
-    gulp.watch('./assets/js/*', gulp.series('dev-minify:js'));   
+  gulp.watch("./assets/css/*", gulp.series("dev-minify:css"));
+  gulp.watch("./assets/js/*", gulp.series("dev-minify:js"));
 
-    gulp.watch('./dist/**/*').on('change', browserSync.reload);
+  gulp.watch("./dist/**/*").on("change", browserSync.reload);
 });
 
-gulp.task('dev-makebuild', run('npm run dev-makebuild'));
+gulp.task("dev-makebuild", run("npm run dev-makebuild"));
 
-gulp.task('dev-postbuild', run('npm run dev-postbuild'));
-gulp.task('dev-minify:css', run(['npm run dev-copy-assets','npm run dev-minify:css']));
-gulp.task('dev-minify:js', run(['npm run dev-copy-assets', 'npm run dev-minify:js']));
+gulp.task("dev-postbuild", run("npm run dev-postbuild"));
+gulp.task(
+  "dev-minify:css",
+  run(["npm run dev-copy-assets", "npm run dev-minify:css"])
+);
+gulp.task(
+  "dev-minify:js",
+  run(["npm run dev-copy-assets", "npm run dev-minify:js"])
+);
 
-gulp.task('default', gulp.series('dev-makebuild', 'dev-postbuild', 'serve'));
+gulp.task("default", gulp.series("dev-makebuild", "dev-postbuild", "serve"));
